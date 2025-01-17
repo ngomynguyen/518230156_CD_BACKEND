5c7b1a78e296a2f7917abba8a2d03974f384be0f
unknown <ngomynguyen0105@gmail.com>
2025-01-17 11:35:58 +0700

    create category

--- src/app.js
+++ src/app.js
@@ -2,6 +2,7 @@ import express from "express";
 import path from "path";
 import routers from "./routers/index.js";
 import mongoConnecter from "./mongo/mongoConnecter.js";
+import bodyParser from "body-parser";
 const app = express();
 const port = 5001;
 const __dirname = path.resolve();
@@ -11,6 +12,8 @@ app.use("/static", express.static(path.join(__dirname, "public")));
 
 app.set("view engine", "ejs");
 app.set("views", __dirname + "/src/views");
+app.use(bodyParser.json())
+app.use(bodyParser.urlencoded())
 routers(app)
 
 
--- src/controllers/categoryController.js
+++ src/controllers/categoryController.js
@@ -12,4 +12,22 @@ export async function listCategory(req, res) {
     res.send("hiện tại không có sản phẩm nào!")
   }
 }
+export async function renderPageCreateCategory(req, res) {
+  res.render("pages/categories/create", {
+    title: "Creat Categories",
+})
+}
+
+export async function createCategory(req, res) {
+  const {code, name, image} = req.body
+  try {
+   await CategoryModel.create({
+      code, name, image, createdAt: new Date()
+    })
+    res.redirect("/categories")
+  } catch (error) {
+    console.log(error)
+    res.send("tạo loại sản phẩm không thành công!")
+  }
+}
 
